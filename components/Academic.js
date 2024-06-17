64aa43e80b761c24cfac05f31bf881182f71b372
kristerus <kristerus@protonmail.com>
2024-06-17 02:05:36 +0200

    fullcode

--- components/Academic.js
+++ components/Academic.js
@@ -3,12 +3,46 @@ import styles from '../app/page.module.css'
 import Image from 'next/image'
 
 
-export const Academic = (title,description,imageLink) => {
+export const Academic = () => {
   return (
     <div id="#academic">
-      <h1>Academic</h1>
-      <p>Academic description</p>
-      <Image src="/academic.jpg" alt="Academic" width={10 } height={10} />
+      <h1 style={{textAlign:"center",marginTop:"20px"}}>Academic Projects</h1>
+      <div className={styles.projectcontainer}>
+        <div className={styles.project}>
+          <a href="https://replit.com/@KristianDhimitr/Time-Series-Analysis-on-Stock-Prices">
+            <h4>Project 1</h4>
+          <h2><a href="https://replit.com/@KristianDhimitr/Time-Series-Analysis-on-Stock-Prices">Time Series Analysis on Stock Prices</a></h2>
+          <p><a href="https://replit.com/@KristianDhimitr/Time-Series-Analysis-on-Stock-Prices">Time series analysis on stock prices can reveal valuable insights for investors and analysts alike.</a></p>
+        </a>
+        </div>
+        <div className={styles.project}>
+          <a href="https://replit.com/@KristianDhimitr/Clustering-Analysis-on-Iris-Dataset">
+<h4>Project 2</h4>
+            <h2><a href="https://replit.com/@KristianDhimitr/Clustering-Analysis-on-Iris-Dataset">Clustering Analysis on Iris Dataset</a></h2>
+          <p><a href="https://replit.com/@KristianDhimitr/Clustering-Analysis-on-Iris-Dataset">Performing clustering analysis on the Iris dataset involves several steps: loading the dataset, preprocessing if necessary, applying K-means clustering, visualizing the clusters, and evaluating the clustering model.</a></p>
+       </a>
+        </div>
+        <div className={styles.project}><a href="https://replit.com/@KristianDhimitr/Natural-Language-Processing-NLP-on-Sentiment-Analysis">
+<h4>Project 3</h4>
+          <h2><a href="https://replit.com/@KristianDhimitr/Natural-Language-Processing-NLP-on-Sentiment-Analysis">Natural Language Processing (NLP) on Sentiment Analysis</a></h2>
+          <p><a href="https://replit.com/@KristianDhimitr/Natural-Language-Processing-NLP-on-Sentiment-Analysis">The objective of this project is to perform sentiment analysis on IMDb movie reviews using machine learning techniques, specifically using a Naive Bayes classifier.</a></p>
+        </a>
+        </div>
+        <div className={styles.project}>
+          <a href="https://replit.com/@KristianDhimitr/Exploratory-Analysis-on-Titanic-Dataset">
+          <h4>Project 4</h4>
+          <h2><a href="https://replit.com/@KristianDhimitr/Exploratory-Analysis-on-Titanic-Dataset">Exploratory Data Analysis on Titanic Dataset</a></h2>
+          <p><a href="https://replit.com/@KristianDhimitr/Exploratory-Analysis-on-Titanic-Dataset">The code analyzes the Titanic dataset for insights using visualizations, handles missing values, encodes categorical data, and prepares it for machine learning.</a></p>
+       </a>
+        </div>
+        <div className={styles.project}>
+          <a href="https://replit.com/@KristianDhimitr/Linear-Regression-Analysis-on-Housing-Prices#main.py">
+            <h4>Project 5</h4>
+            <h2><a href="https://replit.com/@KristianDhimitr/Linear-Regression-Analysis-on-Housing-Prices#main.py">Linear Regression Analysis on Housing Prices</a></h2>
+          <p><a href="https://replit.com/@KristianDhimitr/Linear-Regression-Analysis-on-Housing-Prices#main.py">Performing linear regression to sort out the proper price trend in a housing Dataset, based in the Californian Housing dataset.</a></p>
+      </a>
+        </div>
+      </div>
     </div>
-  )
+  ); // Add a closing curly brace here
 }
--- components/Industry.js
+++ components/Industry.js
@@ -4,7 +4,38 @@ import styles from '../app/page.module.css'
 
 export const Industry = (title, description, imageLink) => {
   return (
-    <div id="#industry">Industry</div>
+    <div id="#industry">
+      <h1 style={{ textAlign: "center", marginTop: "20px" }}>Industry Related Projects</h1>
+      <div className={styles.projectcontainer}>
+        <div className={styles.project}>
+          <a href="https://replit.com/@KristianDhimitr/Customer-Segmentation-using-Clustering">
+            <h4>Project 1</h4>
+            <h2><a href="https://replit.com/@KristianDhimitr/Customer-Segmentation-using-Clustering">Customer Segmentation using Clustering</a></h2>
+            <p><a href="https://replit.com/@KristianDhimitr/Customer-Segmentation-using-Clustering">To perform customer segmentation using clustering techniques like K-means on a retail store dataset, we'll go through several steps including data exploration, preprocessing, clustering, and deriving insights. </a></p>
+          </a>
+        </div>
+        <div className={styles.project}>
+          <a href="https://replit.com/@KristianDhimitr/Churn-Prediction-for-a-Telecom-Company#main.py">
+            <h4>Project 2</h4>
+            <h2><a href="https://replit.com/@KristianDhimitr/Churn-Prediction-for-a-Telecom-Company#main.py">Churn Prediction for a Telecom Company</a></h2>
+            <p><a href="https://replit.com/@KristianDhimitr/Churn-Prediction-for-a-Telecom-Company#main.py">"Generates synthetic telecom customer data, builds and evaluates a Random Forest Classifier to predict churn, and visualizes feature importances."</a></p>
+          </a>
+        </div>
+        <div className={styles.project}><a href="https://replit.com/@KristianDhimitr/SnowTepidObservatory">
+          <h4>Project 3</h4>
+          <h2><a href="https://replit.com/@KristianDhimitr/SnowTepidObservatory">Sales Forecasting for a Retail Store</a></h2>
+          <p><a href="https://replit.com/@KristianDhimitr/SnowTepidObservatory">Python code for performing sales forecasting using SARIMA model and visualizing the results with Matplotlib.</a></p>
+        </a>
+        </div>
+        <div className={styles.project}>
+          <a href="https://replit.com/@KristianDhimitr/Sentiment-Analysis-on-Social-Media-Data#main.py">
+            <h4>Project 4</h4>
+            <h2><a href="https://replit.com/@KristianDhimitr/Sentiment-Analysis-on-Social-Media-Data#main.py">Sentiment Analysis on Social Media Data</a></h2>
+            <p><a href="https://replit.com/@KristianDhimitr/Sentiment-Analysis-on-Social-Media-Data#main.py">Performing sentiment analysis on tweets related to the company "Nova" using a logistic regression model trained on TF-IDF transformed text data, with subsequent classification and visualization of sentiment distribution.</a></p>
+          </a>
+        </div>
+      </div>
+    </div>
   )
 }
 
